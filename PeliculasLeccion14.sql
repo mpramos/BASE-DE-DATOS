b75081b125a2a8dd3d013ced7f85dfed28432244
maria <mpramos78915400@gmail.com>
2024-02-22 21:36:34 -0400

    leccion 14 , update

---
+++ PeliculasLeccion14.sql
@@ -0,0 +1,5 @@
+-- Ejercicio 14: Tareas
+
+-- 1. El director de Bichos está equivocado, en realidad fue dirigida por **John Lasseter**
+-- 2. El año en que se lanzó Toy Story 2 es incorrecto, en realidad se lanzó en **1999.**
+-- 3. ¡Tanto el título como el director de Toy Story 8 son incorrectos! El título debería ser "Toy Story 3" y fue dirigida por **Lee Unkrich**
